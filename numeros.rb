00d742faced421e93e38c1ac1fc5fc0ef9ece1f2
Juan Carlos Gutierrez <jc.gutierrezs@gmail.com>
2022-05-04 21:37:50 -0400

    patron de piramide con numeros

--- numeros.rb
+++ numeros.rb
@@ -9,19 +9,7 @@ end
 
 numeres(ARGV[0].to_i)
 
-def numeres(numeres)
-    for i in (1..numeres)  #i representa las filas
-        for j in(1..i)  #j representa las columnas
-            print j 
-        end
-        print "\n"
-    end
-end
 
-numeres(ARGV[0].to_i)
-
-
-n = ARGV[0].to_i
 
 
 
